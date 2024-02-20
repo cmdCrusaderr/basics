82084478c0f4d74eaad2db4225a277bf93bc0421
nishant tekwani <nishanttekwani03@gmail.com>
2024-02-20 06:16:22 +0530

    Initial Commit

---
+++ basics/basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicsApp.swift
+//  basics
+//
+//  Created by NISHANT TEKWANI on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
